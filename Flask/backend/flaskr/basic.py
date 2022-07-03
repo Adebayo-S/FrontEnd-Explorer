9a36f00860ad06340e61e6410b54b22d66e61cad
Adebayo Samuel <eladebayoor@gmail.com>
2022-07-04 00:38:43 +0100

    [tests]: test files added

--- Flask/backend/flaskr/basic.py
+++ Flask/backend/flaskr/basic.py
@@ -39,16 +39,4 @@ def create_app(test_config=None):
 
     return app
 
-# working in a virtual environment
-# > py -3 -m venv venv
-# > venv\Scripts\activate
 
-# In bash terminal
-# export FLASK_APP=Flask
-# export FLASK_ENV=development (whenever we make mods, the server restarts)
-# flask run
-
-# In powershell terminal
-# > $env:FLASK_APP = "flaskr"
-# > $env:FLASK_ENV = "development"
-# > flask run
